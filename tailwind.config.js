bf789a1f1530e210502a587eac2aa2476b22cfcf
Burhan Rabbani <burhanit11@gmail.com>
2024-07-15 09:34:54 -0700

    strature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cora: "#ff7a4f",
+      },
+    },
   },
   plugins: [],
-}
+};
